041bbdc05c684e38f5d43d06140e7b80634bc5d5
baschi29 <baschi29@yahoo.de>
2020-08-23 17:16:34 +0200

    Closes #3

--- dpSite/dpSite/settings.py
+++ dpSite/dpSite/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+env = environ.Env()
+eviron.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n=9%2g^oud2doq&chn9^e402i9s#+c67()ubkom#+_(erck-u#'
+SECRET_KEY: = env(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
